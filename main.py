a624ca96132d34a82ee9de370e5b6c162cace80f
Franco Speziali <franco@wemakemachines.com>
2021-11-28 14:09:42 +0100

    Added verbose output

--- main.py
+++ main.py
@@ -9,19 +9,23 @@ memcached = Memcached()
 periodic_runner = PeriodicRunner()
 
 
-def run(name: str, url: str):
+def run(name: str, url: str, interval: int):
+    print(f"Making a request to {url}")
     response = requests.get(url)
     memcached.set(name, json.dumps(response.text))
+    print(f"Response saved under key {name}")
+    print(f"Will run again in {interval} seconds...")
 
 
 def app():
+    print("Initialising periodic-sync-service...")
     for sync_points in BaseConfig.SYNC_POINTS:
         name = sync_points["name"]
         url = sync_points["url"]
         interval = sync_points["interval"]
 
         def _job():
-            run(name, url)
+            run(name, url, interval)
 
         periodic_runner.set_job(name, interval, _job)
 
